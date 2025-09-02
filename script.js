20587ac34801ef95bff40d36e8e78d98f07df48a
IzamAhmed786 <izamahmed786@gmail.com>
2025-09-02 10:27:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let butn=document.querySelector('input[type="button"]');
+// let set=document.getElementById("colorSelect");
+
+butn.addEventListener("click",removeColor);
+
+function removeColor() {
+      const select = document.getElementById("colorSelect");
+      
+      if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+      }
+    }
